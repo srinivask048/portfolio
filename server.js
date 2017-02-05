4d2b112dc3ed14a535a42693a4ca0730feedd64d
Srini K <SriniK@Srinivasas-MacBook-Pro.local>
2017-02-05 14:27:10 -0800

    adding node server support

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var path = require("path");
 
 var app = express();
 
-app.set('port', 4000);
-
+app.set('port', 4001);
 //Set static path:
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(__dirname));
 
 app.listen(app.get('port'), function(){
-	console.log('listening on port:'+app.get('port'));
+	console.log('listening on port:'+app.get('port')+"\n:path:::"+__dirname);
 });
