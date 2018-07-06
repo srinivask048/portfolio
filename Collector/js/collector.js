670e7978859c7d0891ddfb39e148b2c6e227e02b
Srinivasa Raju Kalidindi (Srini K) <srinivask048@gmail.com>
2018-07-06 14:09:31 -0400

    adding mobile support for laser on click event
    adding mobile support for laser on click event

--- Collector/js/collector.js
+++ Collector/js/collector.js
@@ -1,6 +1,16 @@
-var collector = (function(){
-    "use strict";
-    return {
+var laserModule = (function(){
+    "use strict"
+   
+    function laserMouseClickHandler(event){
+        if(event.currentTarget.classList.contains("hovered")){
+            event.currentTarget.classList.remove("hovered");
+        }else{
+            event.currentTarget.classList.add("hovered");
+        }
         
+    }
+    
+    return {
+        laserMouseClickHandler : laserMouseClickHandler
     };
 }());
